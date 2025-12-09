d4ab7b33099f82ddde1f73f42d7fb52dbffd16d1
TheP4nda-1 <121826004+TheP4nda-1@users.noreply.github.com>
2025-12-09 16:50:41 +0100

    Set up Express static file serving

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 
